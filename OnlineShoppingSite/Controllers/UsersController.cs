052c2a4d9850177f81685514a0ef6f1693a75006
siddharth shirodkar <siddharthshirodkar36@gmail.com>
2021-03-04 08:59:26 +0530

    net core project

---
+++ OnlineShoppingSite/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingSite.Data;
+using OnlineShoppingSite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingSite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //making use of async , multithreading feature
+        public  async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();//tolistAsync from EntityFrameWorkCore
+            //return _context.Users.ToListAsync().Result
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ OnlineShoppingSite/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShoppingSite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingSite.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options ):base(options)
+        {
+                
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
